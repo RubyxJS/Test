0b637e98caea6fc604ae5e49cabca89d2c48ec2e
Reuben <68828483+RubyxJS@users.noreply.github.com>
2020-11-17 22:53:10 +0000

    change

--- School/src/main/java/Assignment.java
+++ School/src/main/java/Assignment.java
@@ -1,6 +1,8 @@
 class HeightCalculator {
     public static void main(String[] args) {
-        
+
+        // makes the code loop when an option is complete
+        // asks the end user what task they want to carry out
         while (true) {
             System.out.println("Enter 1 to calculate average");
             System.out.println("Enter 2 to calculate total");
@@ -9,10 +11,13 @@ class HeightCalculator {
             System.out.println("Enter option:");
 
             int option = Console.readInt();
+            // if 4 is entered for "option" the program ends
             if (option >= 4) {
                 System.out.println("Program closed");
                 break;
             } else if (option == 1) {
+                // if 1 is entered for option the user is asked for the sum of the heights and
+                // the amount of people, and is given the average
                 System.out.println("Enter Sum of heights:");
                 double sumHeight = Console.readInt();
 
@@ -23,8 +28,12 @@ class HeightCalculator {
                 System.out.print("The Average is = ");
                 System.out.println(heightAverage);
                 System.out.println("   ");
+                System.out.println("******** NEW CHIOCE ********");
+
 
             } else if (option == 2) {
+                // if 2 is entered for "option" the user is asked the amount of people and then
+                // has to put in all the height values, and the Height total
                 System.out.println("Enter amount of people:");
                 int sumPeople = Console.readInt();
                 if (sumPeople <= 0) {
@@ -32,25 +41,30 @@ class HeightCalculator {
                 } else {
                     double totalHeight = 0;
                     for (int i = 1; i <= sumPeople; i++) {
-                        System.out.println("Insert Height for person "+i+": ");
-                        totalHeight += Console.readInt(); 
+                        System.out.println("Insert Height for person " + i + ": ");
+                        totalHeight += Console.readInt();
                     }
                     System.out.print("The Height Total = ");
                     System.out.println(totalHeight);
                     System.out.println("   ");
+                    System.out.println("******** NEW CHIOCE ********");
 
                 }
 
             } else if (option == 3) {
+                // if 3 is entered for "option" the user is asked the average
+                // then the amount of people is given
                 System.out.println("Enter average height: ");
                 double heightAverage = Console.readInt();
 
                 System.out.println("Enter height total: ");
                 double heightTotal = Console.readInt();
 
-                double heightQuantity = heightTotal/heightAverage;
+                double heightQuantity = heightTotal / heightAverage;
                 System.out.println("Height Quantity = " + heightQuantity);
                 System.out.println("   ");
+                System.out.println("******** NEW CHIOCE ********");
+                
             }
         }
     }
